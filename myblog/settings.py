06e107ba78d02826824f68d82534939224352e0d
Oladoke Damilola <122222424+oladokedamilola@users.noreply.github.com>
2025-01-06 14:29:29 +0100

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -15,10 +15,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['blog-website-k7ei.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 SITE_ID = 1
