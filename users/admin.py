2174287d77b3ad9703846f5cd714316045525b0a
Joshdammy22 <oladokedamilola7@gmail.com>
2025-01-06 18:43:53 +0100

    Updated admin.py in users app

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, UserActivity, UserBlogInteraction, UserSettings
+from .models import Profile
 
 
 @admin.register(Profile)
@@ -10,25 +10,4 @@ class ProfileAdmin(admin.ModelAdmin):
     ordering = ('user__username',)
 
 
-@admin.register(UserActivity)
-class UserActivityAdmin(admin.ModelAdmin):
-    list_display = ('user', 'last_login_time', 'last_activity_time')
-    search_fields = ('user__username',)
-    list_filter = ('last_login_time', 'last_activity_time')
-    ordering = ('-last_activity_time',)
 
-
-@admin.register(UserBlogInteraction)
-class UserBlogInteractionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'blog', 'is_favorite', 'liked', 'commented', 'last_interaction')
-    search_fields = ('user__username', 'blog__title')
-    list_filter = ('is_favorite', 'liked', 'commented', 'last_interaction')
-    ordering = ('-last_interaction',)
-
-
-@admin.register(UserSettings)
-class UserSettingsAdmin(admin.ModelAdmin):
-    list_display = ('user', 'email_notifications', 'dark_mode')
-    search_fields = ('user__username',)
-    list_filter = ('email_notifications', 'dark_mode')
-    ordering = ('user__username',)
